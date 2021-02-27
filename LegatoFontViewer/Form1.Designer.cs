45d01f60148ccfef9b69720f17619b46938f0c4a
Joe Ostrander <2170653+joeostrander@users.noreply.github.com>
2021-02-27 11:19:07 -0600

    Initial commit

---
+++ LegatoFontViewer/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace LegatoFontViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonGetFonts = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.textBoxFontInfo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonGetFonts
+            // 
+            this.buttonGetFonts.Location = new System.Drawing.Point(363, 23);
+            this.buttonGetFonts.Name = "buttonGetFonts";
+            this.buttonGetFonts.Size = new System.Drawing.Size(60, 23);
+            this.buttonGetFonts.TabIndex = 3;
+            this.buttonGetFonts.Text = "&Load";
+            this.buttonGetFonts.UseVisualStyleBackColor = true;
+            this.buttonGetFonts.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 253);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Enabled = false;
+            this.numericUpDown1.Location = new System.Drawing.Point(361, 80);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "le_gen_fonts.c";
+            this.openFileDialog1.Filter = "C Files|*.c";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path to *fonts.c file:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 79);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(340, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select Font:";
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(295, 23);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(60, 23);
+            this.buttonBrowse.TabIndex = 2;
+            this.buttonBrowse.Text = "&Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // textBoxFontInfo
+            // 
+            this.textBoxFontInfo.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFontInfo.Location = new System.Drawing.Point(15, 106);
+            this.textBoxFontInfo.Multiline = true;
+            this.textBoxFontInfo.Name = "textBoxFontInfo";
+            this.textBoxFontInfo.ReadOnly = true;
+            this.textBoxFontInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxFontInfo.Size = new System.Drawing.Size(406, 112);
+            this.textBoxFontInfo.TabIndex = 8;
+            this.textBoxFontInfo.WordWrap = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(358, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Select Glyph:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox2.Location = new System.Drawing.Point(221, 253);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Glyph Actual Size:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(218, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Glyph Enhanced:";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(83, 26);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(82, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 480);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxFontInfo);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonGetFonts);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGetFonts;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.TextBox textBoxFontInfo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ LegatoFontViewer/Form1.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LegatoFontViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public struct my_glyph_data
+        {
+            public UInt16 codepoint;
+            public UInt16 width;
+            public UInt16 height;
+            public UInt16 advance;
+            public UInt16 bearingX;
+            public UInt16 bearingY;
+            public UInt16 flags;
+            public UInt16 data_row_width;
+            public UInt16 data_table_offset;
+            public UInt16 unused;
+        }
+
+        private Dictionary<string, string> dict_fonts;
+
+        private bool SelectFontFile()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void GetFonts()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                if (!SelectFontFile())
+                    return;
+            }
+            if (!System.IO.File.Exists(textBox1.Text))
+            {
+                MessageBox.Show("File not found:\n\n" + textBox1.Text, "Not found", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            comboBox1.Items.Clear();
+            comboBox1.Enabled = false;
+            numericUpDown1.Enabled = false;
+            dict_fonts = new Dictionary<string, string>();
+            textBoxFontInfo.Text = "";
+            numericUpDown1.Value = 0;
+
+            string file_contents = System.IO.File.ReadAllText(textBox1.Text).Replace("\r\n", "\n"); ;
+
+            string pattern = @"const uint8_t (?<font_name>[\S]+)_data\[(?<font_data_totalbytes>[0-9]+)\] =[^{]+{(?<font_data>[^}]+)}";
+
+            Regex regex = new Regex(pattern, RegexOptions.Multiline);
+            MatchCollection colMatches = regex.Matches(file_contents);
+
+            if (colMatches.Count == 0)
+            {
+                //TODO:  status bar?
+                Console.WriteLine("Could not get font data!");
+                return;
+            }
+
+            comboBox1.Enabled = true;
+            numericUpDown1.Enabled = true;
+
+            foreach (Match match in colMatches)
+            {
+                string font_name = match.Groups["font_name"].Value.ToString();
+                string font_data_totalbytes = match.Groups["font_data_totalbytes"].Value.ToString();
+                string font_data = match.Groups["font_data"].Value.ToString();
+                Console.WriteLine("Font name:   {0}", font_name);
+                Console.WriteLine("Total bytes: {0}", font_data_totalbytes);
+                //Console.WriteLine("Byte data:   {0}", font_data);
+
+                var bytes = HexStringToByteArray(font_data);
+
+                comboBox1.Items.Add(font_name);
+
+                string font_info = get_font_info(font_name, file_contents);
+
+                font_info = font_info.Replace("\r\n", "\n").Replace("\n", "\r\n");
+
+                dict_fonts.Add(font_name, font_info);
+                
+            }
+                    
+        }
+
+        private string get_font_info(string font_name, string file_contents)
+        {
+
+
+            string pattern = @"\/\*[^\/]+"+font_name+@"$[^\/]+\*\/";
+            //Console.WriteLine("Pattern: {0}!", pattern);
+
+            Regex regex = new Regex(pattern, RegexOptions.Multiline);
+
+            MatchCollection colMatches = regex.Matches(file_contents);
+
+            if (colMatches.Count == 0)
+            {
+                Console.WriteLine("No font info for {0}!", font_name);
+                return "";
+            }
+                
+
+            return colMatches[0].Value.ToString();
+
+        }
+
+        private void get_glyph()
+        {
+            string selected_font = comboBox1.GetItemText(comboBox1.SelectedItem);
+
+            if (string.IsNullOrEmpty(selected_font))
+                return;
+
+            string file_contents = System.IO.File.ReadAllText(textBox1.Text).Replace("\r\n","\n");
+
+            string pattern = @"const uint8_t "+ selected_font + @"_data\[(?<font_data_totalbytes>[0-9]+)\] =[^{]+{(?<font_data>[^}]+)}";
+
+            Console.WriteLine(pattern);
+            
+            Regex regex = new Regex(pattern, RegexOptions.Multiline);
+            MatchCollection colMatches = regex.Matches(file_contents);
+
+            if (colMatches.Count == 0)
+            {
+                //TODO:  status bar?
+                Console.WriteLine("Could not get font data!");
+                return;
+            }
+
+            Match match = colMatches[0];
+            string font_data_totalbytes = match.Groups["font_data_totalbytes"].Value.ToString();
+            string font_data = match.Groups["font_data"].Value.ToString();
+            Console.WriteLine("Total bytes: {0}", font_data_totalbytes);
+            //Console.WriteLine("Byte data:   {0}", font_data);
+
+            var bytes = HexStringToByteArray(font_data);
+
+            // First 2 bytes are length
+            int total_glyphs = BitConverter.ToInt16(bytes, 0);
+            Console.WriteLine("total_glyphs:  {0}", total_glyphs);
+            numericUpDown1.Maximum = total_glyphs > 1 ? total_glyphs - 1 : 0;
+
+            //Console.WriteLine("!!!!!!! match:   {0}", font_name);
+            //TEST!  get the '!'
+            my_glyph_data glyph1 = RawDeserialize<my_glyph_data>(bytes, 4 + Marshal.SizeOf(typeof(my_glyph_data)) * (int)numericUpDown1.Value);
+            //4, 24, 44, 64
+            Console.WriteLine("codepoint:  {0}", glyph1.codepoint);
+            Console.WriteLine("width:  {0}", glyph1.width);
+            Console.WriteLine("height:  {0}", glyph1.height);
+            Console.WriteLine("data_row_width:  {0}", glyph1.data_row_width);
+            Console.WriteLine("data_table_offset:  {0}", glyph1.data_table_offset);
+            draw_glyph(file_contents, selected_font, (int)glyph1.width, (int)glyph1.height, (int)glyph1.data_row_width, (int)glyph1.data_table_offset);
+
+        }
+
+        private void draw_glyph(string file_contents, string font_name, int width, int height, int data_row_width, int data_table_offset)
+        {
+            string pattern = @"const uint8_t "+font_name+@"_glyphs\[(?<glyph_data_totalbytes>[0-9]+)\] =[^{]+{(?<glyph_data>[^}]+)}";
+
+            Regex regex = new Regex(pattern, RegexOptions.Multiline);
+            MatchCollection colMatches = regex.Matches(file_contents);
+
+            if (colMatches.Count == 0)
+            {
+                Console.WriteLine("Could not get glyph data!");
+                pictureBox1.Image = null;
+                pictureBox2.Image = null;
+                return;
+            }
+
+            if (width < 1 || height < 1)
+            {
+                Console.WriteLine("Bad data!");
+                pictureBox1.Image = null;
+                pictureBox2.Image = null;
+                return;
+            }
+
+            Bitmap myBitmap = new Bitmap(width, height);
+
+            Match match = colMatches[0];
+            string glyph_data_totalbytes = match.Groups["glyph_data_totalbytes"].Value.ToString();
+            string glyph_data = match.Groups["glyph_data"].Value.ToString();
+
+            var bytes = HexStringToByteArray(glyph_data);
+
+            Console.WriteLine("glyph_data_totalbytes:  {0}", glyph_data_totalbytes);
+            Console.WriteLine("data_table_offset:  {0}", data_table_offset);
+            Console.WriteLine("data_row_width:  {0}", data_row_width);
+            int blah = height * data_row_width;
+
+            int pos_y = 0;
+            for (int i = data_table_offset; i < data_table_offset + blah; i+=data_row_width)
+            {
+                string row = "";
+                for (int n = 0; n < data_row_width; n++)
+                {
+                    row += Convert.ToString(bytes[i + n], 2).PadLeft(8, '0');
+                }
+
+                for (int n = 0; n < width; n++)
+                {
+                    Color my_color = row.Substring(n, 1) == "1" ? Color.Black : Color.White;
+                    int y = i - data_table_offset;
+                    myBitmap.SetPixel(n, pos_y, my_color);
+
+                }
+
+                pos_y++;
+            }
+
+            pictureBox1.Image = myBitmap;
+
+            using (Bitmap myBitmap_enlarged = (Bitmap)myBitmap.Clone())
+            {
+                if (myBitmap_enlarged == null)
+                {
+                    return;
+                }
+                //InterpolationMode.HighQualityBicubic; // smoother... but not what we want
+                double new_height = (double)myBitmap_enlarged.Height;
+                double new_width = (double)myBitmap_enlarged.Width;
+
+                double ratio = new_height > new_width ? pictureBox2.Height / new_height : pictureBox2.Width / new_width;
+                new_height *= ratio;
+                new_width *= ratio;
+
+                Image stretched = StretchedImage(myBitmap_enlarged, (int)new_width, (int)new_height, InterpolationMode.NearestNeighbor);
+                pictureBox2.Image = stretched;
+
+            }
+
+            Console.WriteLine();
+
+        }
+
+
+        private Image StretchedImage(Image imgPhoto, int Width, int Height, InterpolationMode interpolation_mode)
+        {
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            int sourceX = 0;
+            int sourceY = 0;
+            int destX = 0;
+            int destY = 0;
+
+
+            Bitmap bmPhoto = new Bitmap(Width, Height, PixelFormat.Format32bppArgb);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution,
+                             imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            //grPhoto.Clear(Color.Red);
+            grPhoto.Clear(Color.Empty);
+
+            grPhoto.InterpolationMode = interpolation_mode;
+
+            grPhoto.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, Width, Height),
+                new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            return bmPhoto;
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            SelectFontFile();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetFonts();
+        }
+
+
+        private static byte[] HexStringToByteArray(string hexstring)
+        {
+            hexstring = hexstring.Replace("0x", "");
+            hexstring = Regex.Replace(hexstring, "[^0-9A-F]", "", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+            byte[] bytes = new byte[hexstring.Length / 2];
+
+            for (int i = 0; i < hexstring.Length - 1; i += 2)
+            {
+                string tmp = hexstring.Substring(i, 2);
+                bytes[i / 2] = Convert.ToByte(tmp, 16);
+            }
+            return bytes;
+        }
+
+        public static T RawDeserialize<T>(byte[] rawData, int position)
+        {
+            int rawsize = Marshal.SizeOf(typeof(T));
+            if (rawsize > rawData.Length - position)
+                throw new ArgumentException("Not enough data to fill struct. Array length from position: " + (rawData.Length - position) + ", Struct length: " + rawsize);
+            IntPtr buffer = Marshal.AllocHGlobal(rawsize);
+            Marshal.Copy(rawData, position, buffer, rawsize);
+            T retobj = (T)Marshal.PtrToStructure(buffer, typeof(T));
+            Marshal.FreeHGlobal(buffer);
+            return retobj;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            get_glyph();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = Application.ProductName;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selected_font = comboBox1.GetItemText(comboBox1.SelectedItem);
+            string value = "";
+
+            if (!string.IsNullOrEmpty(selected_font))
+            {
+                if (dict_fonts.TryGetValue(selected_font, out value))
+                {
+                    Console.WriteLine("For key = \""+ selected_font+"\", value = {0}.", value);
+                }
+            }
+
+            textBoxFontInfo.Text = value;
+            numericUpDown1.Value = 0;
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+        }
+
+        private void buttonGetGlyphs_Click(object sender, EventArgs e)
+        {
+            get_glyph();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 about = new AboutBox1();
+            about.ShowDialog();
+
+        }
+    }
+}
